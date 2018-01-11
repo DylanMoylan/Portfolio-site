3275a84d0c8ceea0440f9140b0a221b6c380811b
Dylan Moylan <dylanmoylan1@gmail.com>
2018-01-11 15:35:50 -0500

    Fixes styling for project icons and moves icon files to the public folder

--- portfolio/src/assets/text-data.js
+++ portfolio/src/assets/text-data.js
@@ -2,7 +2,7 @@ const textData = [
   {
     name: 'SELECTIM',
     github: null,
-    icon: '../assets/selectim.jpg',
+    icon: 'focus-group-icon.png',
     summary: '',
     description: '',
     images: []
@@ -10,7 +10,7 @@ const textData = [
   {
     name: 'Standup!',
     github: null,
-    icon: '../assets/standup.jpg',
+    icon: 'focus-group-icon.png',
     summary: '',
     description: '',
     images: []
@@ -18,7 +18,7 @@ const textData = [
   {
     name: 'Focus Groups App',
     github: null,
-    icon: '../assets/standup.jpg',
+    icon: 'focus-group-icon.png',
     summary: 'Focus Group App / Corporate Group Management',
     description: 'Created an app to allow professional or educational groups to graph how they feel about a project or process from day to day, and describe their daily wins and losses (derived from the General Assembly tradition of “Standups”) enabling visualizations of emotional progression over time. The app was written in Javascript, using React on the front­end (with D3, a data visualization library) and Node/Express on the back­end.',
     images: []
@@ -26,7 +26,7 @@ const textData = [
   {
     name: 'Big Fish, Little Fish',
     github: null,
-    icon: '../assets/standup.jpg',
+    icon: 'focus-group-icon.png',
     summary: '',
     description: 'Created a browser­based game in Javascript using the principles of Object Oriented Programming. The user is a fish, who must avoid larger predators and grow larger by preying on smaller fish.',
     images: []
--- portfolio/src/components/Work.js
+++ portfolio/src/components/Work.js
@@ -2,14 +2,20 @@ import React from 'react'
 import WorkItem from './WorkItem'
 import textData from '../assets/text-data.js'
 
+// import FocusGroupsApp from '../assets/focus-group-icon.png'
+
 class Work extends React.Component {
   constructor(props) {
     super(props)
-    this.workItems = textData
     this.state = {
-      activeWorkItem: 0
+      activeWorkItem: 0,
+      workItems: textData.map((el) => {
+        el.hoverState = false
+        return el
+      })
     }
     this.alterActiveWorkItem = this.alterActiveWorkItem.bind(this)
+    this.hoverListItem = this.hoverListItem.bind(this)
   }
 
   alterActiveWorkItem(event, index) {
@@ -18,24 +24,37 @@ class Work extends React.Component {
     })
   }
 
+  hoverListItem(status, index) {
+    this.setState({
+      workItems: this.state.workItems.fill(status, index, index)
+    })
+  }
+
   render() {
     return (
       <div className="work">
+        <WorkItem item={this.state.workItems[this.state.activeWorkItem]} />
         <div className="item-list">
           <ul>
             {
-              this.workItems.map((el, index) => {
+              this.state.workItems.map((el, index) => {
                 return <li
                           key={el.id}
                           onClick={(event) => {this.alterActiveWorkItem(event, index)}}
+                          onMouseEnter={(e)=> {this.hoverListItem(true, index)}}
+                          onMouseLeave={(e)=> {this.hoverListItem(false, index)}}
+                          style={{backgroundImage: 'url(' + el.icon + ')'}}
                         >
-                        {el.name}
+                          <div
+                            className={ this.state.workItems[index].hoverState ? 'work-item-tooltip' : 'work-item-tooltip hidden' }
+                          >
+                            {el.name}
+                          </div>
                         </li>
               })
             }
           </ul>
         </div>
-        <WorkItem item={this.workItems[this.state.activeWorkItem]} />
       </div>
     )
   }
